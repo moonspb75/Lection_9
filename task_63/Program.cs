216887c2200db9041146b00fb31b7fbd78b3d5be
Yanina <moonspb75@mail.ru>
2023-03-10 22:12:06 +0300

    Seminar_9

---
+++ task_63/Program.cs
@@ -0,0 +1,19 @@
+﻿// // Задача 63: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+void RoadToN(int N)
+{
+    if(N == 0)
+    {
+        return;
+    }
+    RoadToN(N-1);
+    System.Console.Write($"{N} ");
+}
+
+System.Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+RoadToN(number);
---
+++ task_65/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 65: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 6, 7, 8"
+
+void RoadToN(int max, int min )
+{
+    if(max < min)
+    {
+        return;
+    }
+    RoadToN(max-1,min);
+    System.Console.Write($"{max} ");
+}
+
+System.Console.Write("Введите число max: ");
+int numberMax = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число min: ");
+int numberMin = Convert.ToInt32(Console.ReadLine());
+RoadToN(max:numberMax,min:numberMin);
